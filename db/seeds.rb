459f33d54f1667875fc18bd427f6aa3387c9d41b
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-03-21 13:16:59 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+2.times do
+  for i in 1..13 do
+    Card.new(number: i, color: "#E41414")
+    Card.new(number: i, color: "#FF8C00")
+    Card.new(number: i, color: "#0000FF")
+    Card.new(number: i, color: "#000000")
+  end
+end
